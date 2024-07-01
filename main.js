99ae0c5d9350785e04ce6f99907843d363cc505e
Lubna <muspaksamsu@gmail.com>
2024-07-01 23:01:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+//print a message indicating the number of people you are inviting to dinner.
+var guestList = ["saima", "rozina", "saba"];
+var lengthGuests = guestList.length;
+console.log("we are inviteing total ".concat(lengthGuests, " guests"));
---
+++ main.ts
@@ -0,0 +1,6 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+//print a message indicating the number of people you are inviting to dinner.
+
+let guestList : string[] = ["saima","rozina","saba"];
+let lengthGuests : number = guestList.length
+console.log(`we are inviteing total ${lengthGuests} guests`);
